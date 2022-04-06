3a484bb5c2fa45dc20b502bfdb869f70ebe65a61
YILMAZ KAYALI <yilmazanilkayali@gmail.com>
2022-04-06 23:53:43 +0300

    Entity Configurations

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -3,6 +3,7 @@ using NLayer.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +19,12 @@ namespace NLayer.Repository
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //IEntityTypeConfiguration interface inin uygulandığı tüm sınıflar için gerçekleştirir.
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
 
     }
 }
---
+++ NLayer.Repository/Configurations/ProductConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Configurations
+{
+    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(2000);
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x => x.Price).IsRequired().HasColumnType("decimal(18,2)");
+
+
+            builder.ToTable("Products");
+            //modelde custom isimlendirme yapsaydık bu şekilde ilişki verebilirdik. fluent api
+            builder.HasOne(x => x.Category).WithMany(x=> x.Products).HasForeignKey(x=>x.CategoryId);
+        }
+    }
+}
