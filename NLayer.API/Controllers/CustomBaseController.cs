8987a30873fb1ab4faf176972a5d951c719573b0
YILMAZ KAYALI <yilmazanilkayali@gmail.com>
2022-04-08 05:04:27 +0300

    Created ProductsController and CustomBaseController

---
+++ NLayer.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTO;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        [NonAction]  //EndPoint olmadığını belirmek için kullanıldı. kullanılmadığında get mi post mu bilinmediğinden hata fırlatıyor.
+        public IActionResult CreateActionResult<T>(CustomResponseDto<T> response)
+        {
+            if (response.StatusCode == 204)
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.StatusCode
+                };
+            
+                return new ObjectResult(response)
+                {
+                    StatusCode = response.StatusCode
+                };
+        }
+    }
+}
